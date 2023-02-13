8be7be81f79d3208b2ed2a22c282677a75efb780
Henrique Rodrigues <henrique.rodrigues@my-digital-school.org>
2023-02-13 08:46:41 +0100

    le commit sans nano

---
+++ modele/artisteModel.php
@@ -0,0 +1,50 @@
+<?php
+class artisteModel extends Model{
+
+    // Propriétés permettant de personnaliser les requêtes
+    public $table;
+    public $id;
+    private $Connection;
+
+    public function __construct(){
+        $this->table="artistes";
+
+        $this->ConnectionBDD();
+    }
+
+
+    public function createArtiste($artisteNom,$artisteNbMusique){
+        $sql = "INSERT INTO".$this->table."
+        (Id_Artistes,artiste_nom , artiste_nb_musique)
+        VALUES (NULL,'$artisteNom' , '$artisteNbMusique')";
+        $query = $this->$connection->prepare($sql);
+        $query->execute();
+        return $query->fetch(); 
+    }
+
+    public function updateArtisteNom($artisteNom) {
+        $sql = "UPDATE ".$this->table." 
+        SET artistes_nom=".$artisteNom." 
+        WHERE Id_Artistes = ".$this->id ; 
+        $query = $this->$connection->prepare($sql);
+        try{
+            $query->execute();
+        }catch (\Exception $e) {
+            throw new \Exception('Problème lors de l update');
+        }
+        
+    }
+
+    public function updateArtisteNbMusique($artisteNbMusique) {
+        $sql = "UPDATE ".$this->table." 
+        SET artistes_nom=".$artisteNbMusique." 
+        WHERE Id_Artistes = ".$this->id ; 
+        $query = $this->$connection->prepare($sql);
+        try{
+            $query->execute();
+        }catch (\Exception $e) {
+            throw new \Exception('Problème lors de l update');
+        }
+        
+    }
+}
